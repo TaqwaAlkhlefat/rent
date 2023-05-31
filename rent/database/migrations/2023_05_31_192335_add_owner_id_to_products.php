39a8d6b0a3b667ca60a216b9558e30e34eefb544
RaghadNaser-1 <126399047+RaghadNaser-1@users.noreply.github.com>
2023-05-31 12:56:06 -0700

    first_comm

---
+++ rent/database/migrations/2023_05_31_192335_add_owner_id_to_products.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('lessors_id')->nullable();
+            $table->foreign('lessors_id')->references('id')->on('lessors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['lessors_id']);
+            $table->dropColumn('lessors_id');
+        });
+    }
+};
---
+++ rent/routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+
+
+
+// Home Page
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+// About Page
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Services Page
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+// Vehicles Page
+Route::get('/vehicles', function () {
+    return view('vehicles');
+})->name('vehicles');
+
+// Client Page
+Route::get('/client', function () {
+    return view('client');
+})->name('client');
+
+// Contact Page
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+
